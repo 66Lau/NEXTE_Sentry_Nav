df70c020b2c592134ec0d16a127609613ebc3459
66Lau <liuhang20020917@gmail.com>
2023-10-28 17:44:32 +0800

    finish dwa_planner and generate y velocity correctly

---
+++ sentry_nav/src/Trans_TF_2d_odom.cpp
@@ -0,0 +1,44 @@
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
+// tf chage https://zhuanlan.zhihu.com/p/340016739
+
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "Trans_TF_2d_odom");
+
+  ros::NodeHandle node;
+
+  tf::TransformListener listener;
+  tf::TransformBroadcaster broadcaster;
+  tf::Transform transform_broadcaster;
+
+  ros::Rate rate(1000);
+  while (node.ok()){
+    tf::StampedTransform transform_listener;
+    
+    try{
+      listener.lookupTransform("map", "camera_init",  
+                               ros::Time(0), transform_listener);
+    }
+    catch (tf::TransformException ex){
+      ROS_ERROR("%s",ex.what());
+      ros::Duration(1.0).sleep();
+    }
+    float robot_pose_x=transform_listener.getOrigin().x();
+    float robot_pose_y=transform_listener.getOrigin().y();
+    float robot_pose_z=0;
+    float robot_oriation_x=transform_listener.getRotation().getX();
+    float robot_oriation_y=transform_listener.getRotation().getY();
+    float robot_oriation_z=transform_listener.getRotation().getZ();
+    float robot_oriation_w=transform_listener.getRotation().getW();
+
+    transform_broadcaster.setOrigin( tf::Vector3(robot_pose_x, robot_pose_y, 0.0) );
+    transform_broadcaster.setRotation( tf::Quaternion(0, 0, robot_oriation_z, robot_oriation_w) );
+    broadcaster.sendTransform(tf::StampedTransform(transform_broadcaster, ros::Time::now(), "map", "camera_init_2d"));
+    
+
+    rate.sleep();
+  }
+  return 0;
+};
---
+++ sentry_nav/src/Trans_odom_2d.cpp
@@ -0,0 +1,62 @@
+#include "ros/ros.h"  
+
+
+#include <serial/serial.h>  
+#include <std_msgs/String.h>  
+#include <sstream>  
+#include <nav_msgs/Odometry.h>
+#include <string>
+#include <iostream>
+
+
+nav_msgs::Odometry odom_2d;
+
+// 接收到订阅的消息后，会进入消息回调函数
+void callback(const nav_msgs::Odometry& odom)
+{
+    // receive the msg from cmd_vel
+    ROS_INFO("Receive a odom msg\n");
+    // ROS_INFO("The linear  velocity: x=%f, y=%f, z=%f\n",cmd_vel.linear.x,cmd_vel.linear.y,cmd_vel.linear.z);
+    // ROS_INFO("The augular velocity: roll=%f, pitch=%f, yaw=%f\n",cmd_vel.angular.x, cmd_vel.angular.y, cmd_vel.angular.z);
+    // put the data in union
+    odom_2d.header.stamp = ros::Time::now();
+    odom_2d.header.frame_id = "camera_init_2d";
+    odom_2d.child_frame_id = "map";
+
+    odom_2d.pose.pose.position.x = odom.pose.pose.position.x;
+    odom_2d.pose.pose.position.y = odom.pose.pose.position.y;
+    odom_2d.pose.pose.position.z = 0.00;
+    odom_2d.pose.pose.orientation.x = 0.00;
+    odom_2d.pose.pose.orientation.y = 0.00;
+    odom_2d.pose.pose.orientation.z = odom.pose.pose.orientation.z;
+    odom_2d.pose.pose.orientation.w = odom.pose.pose.orientation.w;
+
+    odom_2d.twist.twist.linear.x = odom.twist.twist.linear.x;
+    odom_2d.twist.twist.linear.y = odom.twist.twist.linear.y;
+    odom_2d.twist.twist.linear.z = 0;
+    odom_2d.twist.twist.angular.x = 0;
+    odom_2d.twist.twist.angular.y = 0;
+    odom_2d.twist.twist.angular.z = odom.twist.twist.angular.z;
+
+    
+
+}
+
+
+int main (int argc, char** argv){
+
+    ros::init(argc, argv, "Trans_odom_2d");
+
+    ros::NodeHandle n;
+    ros::Rate r(100);
+    
+
+    // 创建一个Subscriber，订阅名为data_chatter的topic，注册回调函数chatterCallback 
+    ros::Subscriber sub = n.subscribe("/cmd_vel", 1000, callback); 
+    ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom_2d",50);
+    while(ros::ok()){
+        ros::spinOnce();
+        odom_pub.publish(odom_2d);
+        r.sleep();}
+        
+}
